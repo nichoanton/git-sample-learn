23ea33c0ddcc43e2f5823165320ab2910c7094f8
Nicho Antony <58360342+nichoanton@users.noreply.github.com>
2022-10-31 12:36:53 +0530

    updated dummy with hello

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ import React from 'react'
 const app = () => {
   return (
     <>
-    <div>HELLO WORLD!</div>
-    <div>HELLO NICHO!</div>
-    <div>HELLO ANTONY!</div>
-    <div>HELLO PRAKASH!</div>
+    <div>HELLO</div>
     </>
   )
 }
